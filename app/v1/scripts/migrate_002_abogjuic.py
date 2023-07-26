267198db906fbde553d99e56885484859240d81d
fblasco1 <francisco.o.blasco@gmail.com>
2023-07-26 15:55:36 -0300

    Actualizacion git ignore y eliminacion de docker-compose

--- app/v1/scripts/migrate_002_abogjuic.py
+++ app/v1/scripts/migrate_002_abogjuic.py
@@ -6,8 +6,4 @@ from app.v1.model.juicio_model import Juicio_Abogado
 def migrate_002_abogjuic():
     migrator = PostgresqlMigrator(db)
 
-    migrate(
-        migrator.drop_column('juicio', 'responsable_id'),
-    )
-
     db.create_tables([Juicio_Abogado])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.v1.router.user_router import router as user_router
 from app.v1.router.juicio_router import router as juicio_router
 
@@ -7,3 +7,11 @@ app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(juicio_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
